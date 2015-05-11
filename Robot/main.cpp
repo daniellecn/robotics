c04a20258e99990a6f77dbed1c5508641d2c0f1d
colman <colman@ubuntu>
2015-05-11 13:17:26 -0700

    basic movement

--- Robot/main.cpp
+++ Robot/main.cpp
@@ -6,7 +6,8 @@
  */
 
 #include <libplayerc++/playerc++.h>
-
+#include <iostream>
+using namespace std;
 using namespace PlayerCc;
 
 int main() {
@@ -14,16 +15,57 @@ int main() {
 	PlayerClient pc("localhost", 6665);
 	LaserProxy lp(&pc);
 	Position2dProxy pp(&pc);
+	cout << "Anna" << DTOR(40);
+	int avoid = 0;
+	int maxPosObs = 0;
+	int minPosObs = 682;
 
+	bool obs;
 	pp.SetMotorEnable(true);
+
 	while (true) {
 		pc.Read();
+		//cout << lp.GetSize() << endl;
+		obs = false;
+		for (uint i = 241; i < 441; i++)
+		{
+		  if(lp[i] < 0.750)
+			obs = true;
+		  	avoid = 15;
 
-		if (lp[333] < 0.8)
-			pp.SetSpeed(0.0, 0.3);
-		else
+		  	if (i > maxPosObs) {
+		  		maxPosObs = i;
+		  	}
+		  	if (i < minPosObs) {
+		  		minPosObs = i;
+		  	}
+		}
+
+		if (obs && avoid > 0 ) {
+			if (lp.GetMinLeft() < lp.GetMinRight()) {
+				pp.SetSpeed(0.0, -0.6);
+			} else {
+				pp.SetSpeed(0.0, 0.6);
+			}
+			avoid--;
+
+		} else {
+			avoid = 0;
 			pp.SetSpeed(0.8, 0.0);
+		}
+		/*
+		if (lp[3] < 0.8) {
+			pp.SetSpeed(0.0, 0.3);
+		}
+		else if(lp[1]) {
+
+		}
+		else {
+			pp.SetSpeed(0.8, 0.0);
+		}
+		*/
 	}
+
 	return 0;
 
 }
